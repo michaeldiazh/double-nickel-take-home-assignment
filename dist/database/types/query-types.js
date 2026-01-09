335a0d6168ab99087b4c11c6b03cf2e26f595db3
Michael Diaz <michaeldiazh@gmail.com>
2026-01-09 01:32:12 -0500

    deleting git ignore dir

--- dist/database/types/query-types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QUERY_FILTER_KEY = void 0;
-exports.QUERY_FILTER_KEY = [
-    '>', '<', '>=', '<=', '=', '!=', 'in', 'not in', 'like', 'ilike', 'is null', 'is not null'
-];
